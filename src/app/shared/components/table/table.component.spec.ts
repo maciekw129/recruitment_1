2b62601082c39f612eedb10fd963a36d0f8bb3af
Maciej Walecki <maciejwalecki99@gmail.com>
2024-04-20 15:49:02 +0200

    feature: added empty table text

--- src/app/shared/components/table/table.component.spec.ts
+++ src/app/shared/components/table/table.component.spec.ts
@@ -3,15 +3,15 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { TableComponent } from './table.component';
 
 describe('TableComponent', () => {
-  let component: TableComponent;
-  let fixture: ComponentFixture<TableComponent>;
+  let component: TableComponent<any>;
+  let fixture: ComponentFixture<TableComponent<any>>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [TableComponent]
     })
     .compileComponents();
-    
+
     fixture = TestBed.createComponent(TableComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
